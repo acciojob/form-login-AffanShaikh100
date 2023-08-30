b3694a303766919f39815a348171c60db0ff9bbf
AffanShaikh100 <133389464+AffanShaikh100@users.noreply.github.com>
2023-08-30 20:43:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 function getFormvalue() {
     //Write your code here
-
+	let first = document.getElementById("firstn").value;
+	let last = document.getElementById("lastn").value;
+alert(first+" "+last);
 }
